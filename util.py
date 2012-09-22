ac58dc43b73a933898803060938290b57005a249
wentao.zhang <taowenzhang@gmail.com>
2012-09-22 23:17:08 +0800

    add Client class to maintain a account online

--- util.py
+++ util.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 #-*- coding:utf8 -*-
+
 import httplib
 import urllib
 import json
@@ -19,7 +20,6 @@ def get_access_token(client_id,client_secret,usrname,passwd):
 	backen_data =conn.getresponse().read()  
 	obj = json.loads(backen_data)
 	return obj['access_token']
-#	access_token = obj['access_token']
 
 def api_proxy(api_url,param_obj={},method='GET'):
 	conn = httplib.HTTPSConnection("api.weibo.com")  
@@ -39,3 +39,20 @@ def api_proxy_with_access_token(api_url,param_obj={},method='GET'):
 	param_obj_inner.update(param_obj) 
 	obj = api_proxy(api_url,param_obj_inner,method);
 	return obj
+
+def obtain_access_key(client_id,client_secret,usrname,passwd):
+	access_token = get_access_token(client_id,client_secret,usrname,passwd)	
+
+class Client:
+	client_id=''
+	client_secret=''
+	@classmethod
+	def init(cls,client_id,client_secret):
+		cls.client_id = client_id
+		cls.client_secret = client_secret
+	def __init__(self,user_name,pass_word):
+		self.access_token = get_access_token(self.__class__.client_id,self.__class__.client_secret,user_name,pass_word)
+	def proxy_for(self,api_url,param_obj={},method='GET'):
+		param_obj_inner = {'access_token':self.access_token}
+		param_obj_inner.update(param_obj)
+		return api_proxy(api_url,param_obj_inner,method)
