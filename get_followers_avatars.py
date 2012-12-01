45898a0912b565b9705c5d3ec6a5d9eb29123e04
wentao.zhang <taowenzhang@gmail.com>
2012-12-01 23:38:06 +0800

    获取用户粉丝头像

---
+++ get_followers_avatars.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+#-*- coding:utf8 -*-
+import util
+import json
+from urllib import urlretrieve
+keys_obj = util.get_keys("keys.json")
+util.Client.init(keys_obj['client_id'],keys_obj['client_secret'])
+client = util.Client(keys_obj['username1'],keys_obj['password1'])
+ids = client.get_somebody_follower_ids(keys_obj['who_follower_avatar'])
+pic_url_list = []
+for follower_id in ids:
+	print 'id:'+str(follower_id)
+	info = client.get_somebody_info(follower_id)
+	try:
+		pic_url = info['profile_image_url']
+		pic_url_list.append(pic_url)
+		urlretrieve(pic_url,'./heads/'+str(follower_id)+'.jpeg')
+	except:
+		print 'err:'+str(follower_id)
+		print 'msg:'+json.dumps(info)
+		print '\n'
+		continue
+print 'picNum:'+str(len(pic_url_list))
